b7eca6afebcb1d4669d32f729af72b96d26aa34f
bhavanashendge <47981943+bhavanashendge@users.noreply.github.com>
2019-05-02 08:09:11 -0400

    Update run.pp

--- manifests/run.pp
+++ manifests/run.pp
@@ -7,7 +7,11 @@
 class puppet_simple_website::run {
 
   exec {'Docker run':
-        command => '/usr/bin/docker run  -it -d -p 8091:8080 simple-php'
+        command => '/usr/bin/docker stop simple-php-container'
+  }
+
+  exec {'Docker run':
+        command => '/usr/bin/docker run  -it -d -p 80:8080 --name simple-php-container simple-php'
   }
 
 }
