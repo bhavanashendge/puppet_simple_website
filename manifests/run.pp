338d9ecc7f90e223c160c7a31ba90fb8a17cd437
bhavanashendge <47981943+bhavanashendge@users.noreply.github.com>
2019-05-02 08:16:23 -0400

    Update run.pp

--- manifests/run.pp
+++ manifests/run.pp
@@ -6,12 +6,17 @@
 #   include puppet_simple_website::run
 class puppet_simple_website::run {
 
-  exec {'Docker run':
+  exec {'Docker stop old container if existing':
         command => '/usr/bin/docker stop simple-php-container'
   }
+  
+  exec {'Docker prune old containers':
+        command => '/usr/bin/docker container rm simple-php-container'
+  }
+  
 
   exec {'Docker run':
-        command => '/usr/bin/docker run  -it -d -p 80:8080 --name simple-php-container simple-php'
+        command => '/usr/bin/docker run  -it -d -p 80:8080 --name simple-php-container simple-php-image'
   }
 
 }
